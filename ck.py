abfd0b558721ac25fd57ef49609b20b19efcbd0e
Hugh <53116076+hellohellohey@users.noreply.github.com>
2023-07-21 08:52:12 +0800

    Create ck.py

---
+++ ck.py
@@ -0,0 +1,18 @@
+python
+import time
+import sys
+
+total_seconds = 3600 # 专注时钟总秒数,默认1小时
+seconds_passed = 0
+
+while seconds_passed < total_seconds:
+    
+    # 打印倒计时
+    sys.stdout.write("\r") 
+    sys.stdout.write("{:2d}:{:02d}".format((total_seconds-seconds_passed)//60, (total_seconds-seconds_passed)%60))
+    sys.stdout.flush()
+
+    time.sleep(1)
+    seconds_passed += 1
+
+print("\n时间到!")
